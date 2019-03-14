44d09e943a4585f4c97700eb6991f9c09c924c92
Alison Ostendorf <alisonostendorf@Alisons-Air.lan>
2019-03-13 18:18:10 -0700

    add wow.js and custom.js into all files, and remove redundant file wow.js from index header

---
+++ js/custom.js
@@ -0,0 +1,5 @@
+// for building hover of nav icons that reveal the names of those pages, in md and lg
+
+// if hover over ('.sidebar-nav-link-icon') 
+// then
+// show the next following ('.sidebar-nav-link-title')
